2abdf36f4e57b3164394a3688a64e6d9b9ee3792
Teddy Li <teddyxlandlee@hotmail.com>
2026-04-02 19:30:02 +0800

    Initial version of telemetry

---
+++ netlify/edge-functions/telemetry.js
@@ -0,0 +1,90 @@
+import OSS from 'ali-oss';
+import { Base64 } from 'js-base64'
+import { uuidv7 } from 'uuidv7';
+
+/**
+ * @returns {{
+ *   accessKeyId: string,
+ *   accessKeySecret: string,
+ *   bucket: string,
+ * }}
+ */
+function getAliyunOssCredentials() {
+    const environ = Netlify.env.get('ALIYUN_OSS_ACCESS');
+    return JSON.parse(Base64.decode(environ));
+}
+
+const LEVEL_MANDATORY = 0;
+const LEVEL_FUNCTIONAL = 1;
+const LEVEL_OPTIONAL = 2;
+
+/**
+ * 
+ * @param {object} json 
+ * @param {Request} request 
+ * @returns {Response}
+ */
+async function responseV1(json, request, context) {
+    const date = new Date();
+    try {
+        _requires(request.headers.get('Content-Type') === 'application/json');
+        json = checkV1(json, date);
+    } catch {
+        return _fail400();
+    }
+
+    if (json.telemetry_level >= LEVEL_FUNCTIONAL) {
+        json.client_context = {
+            country: context.geo.country,
+            timezone: context.geo.timezone,
+        };
+    }
+    
+    const filename = `${date.getUTCFullYear()}/${date.getUTCMonth()}/${date.getUTCDate()}/${uuidv7()}.json`;
+    
+    const {accessKeyId, accessKeySecret, bucket} = getAliyunOssCredentials();
+    const client = new OSS({
+        accessKeyId, accessKeySecret, bucket,
+        region: 'oss-cn-shanghai',
+        secure: true,
+        authorizationV4: true,
+    });
+    const ossResponse = client.put(filename, Buffer.from(JSON.stringify(json), 'utf-8'), {mime: 'application/json'});
+    context.waitUntil(ossResponse);
+    // Whether response is successful is not concerned by client
+    return new Response(null, {status: 204});
+}
+
+function checkV1(json, dateNow) {
+    const {client_time, mod_version, mod_platform, mc_version} = json
+    _requires(typeof client_time === 'number')
+    _requires(typeof mod_version === 'string')
+    _requires(typeof mc_version === 'string')
+    _requires(['fabric', 'forge', 'neoforge', 'quilt', 'unknown'].includes(mod_platform))
+
+    const ret = {client_time, mod_version, mod_platform, mc_version}
+    ret.client_time = new Date(client_time).toISOString()
+    ret.time = dateNow.toISOString()
+    if (typeof json.telemetry_level !== 'number' || json.telemetry_level < LEVEL_MANDATORY || json.telemetry_level > LEVEL_OPTIONAL) {
+        json.telemetry_level = LEVEL_FUNCTIONAL;
+    }
+
+    return ret
+}
+
+function _fail400(reason = 'Bad Request') {
+    return new Response(reason, {status: 400});
+}
+
+function _requires(precondition, errorMessage = '') {
+    if (!precondition) throw new Error(errorMessage)
+}
+
+export default async (request, context) => {
+    const json = await request.json();
+    if (!json || typeof json !== 'object') return _fail400();
+    // Currently only schema 1 is supported
+    if (json.schema !== 1) return _fail400();
+
+    return responseV1(json, request, context);
+}
